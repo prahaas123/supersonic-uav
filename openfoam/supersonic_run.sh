62dc8ae4469b43521bf7d16ab0e3e45c20632cd3
prahaas123 <kprahaask@gmail.com>
2026-06-06 13:56:26 -0400

    64 cores

--- openfoam/supersonic_run.sh
+++ openfoam/supersonic_run.sh
@@ -4,7 +4,7 @@
 #SBATCH --account=def-jphickey
 #SBATCH --time=6-00:00:00
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=50
+#SBATCH --ntasks-per-node=64
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=150G
 #SBATCH --output=Log.log
